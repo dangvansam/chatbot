1d101f1f11a4cce140f8e63a149715e7bad4baea
Andrew <andrew@techainer.com>
2025-03-04 00:57:28 +0700

    fix

--- api/configs/deploy/__init__.py
+++ api/configs/deploy/__init__.py
@@ -9,7 +9,7 @@ class DeploymentConfig(BaseSettings):
 
     APPLICATION_NAME: str = Field(
         description="Name of the application, used for identification and logging purposes",
-        default="chatbot,
+        default="chatbot"
     )
 
     DEBUG: bool = Field(
